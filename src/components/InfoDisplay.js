07bdff4c609d01932005fa89d60c2ba72b78cb4b
brewchetta <chett.tiller@gmail.com>
2020-09-23 15:48:44 -0400

    Allowed for info to be changed

--- src/components/InfoDisplay.js
+++ src/components/InfoDisplay.js
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 function InfoDisplay() {
 
+  // State
+
   const currentPoint = useSelector(s => s.currentPoint)
+
+  const [nameOpen, setNameOpen] = useState(false)
+
+  const [descOpen, setDescOpen] = useState(false)
+
+  const [name, setName] = useState('')
+
+  const [desc, setDesc] = useState('')
+
+  // Event Handlers
+
+  const handleUnfocus = ({target}) => {
+    if (target.id === "info-name") {
+      console.log(target.value)
+      setNameOpen(!nameOpen)
+    } else if (target.id === "info-description") {
+      console.log(target.value)
+      setDescOpen(!descOpen)
+    }
+  }
+
+  // Render
 
   return (
     <div id="info-display">
 
-      <p>Current Point: {currentPoint.x}, {currentPoint.y}</p>
+      <p id="info-point">Current Point: {currentPoint.x}, {currentPoint.y}</p>
 
-      <h3>Name: {currentPoint.name}</h3>
+      { nameOpen
+        ? <input id="info-name"
+        onChange={(e) => setName(e.target.value)}
+        onBlur={handleUnfocus}
+        type="text" value={name} />
+        : <h3 id="info-name" onClick={() => setNameOpen(!nameOpen)}>Name: {currentPoint.name}</h3>
+      }
 
-      <p> Description: {currentPoint.description}</p>
+      { descOpen
+        ? <input id="info-description"
+        onChange={(e) => setDesc(e.target.value)}
+        onBlur={handleUnfocus}
+        type="text" value={desc} />
+        :<p id="info-description" onClick={() => setDescOpen(!descOpen)}> Description: {currentPoint.description}</p>
+      }
 
     </div>
   )
