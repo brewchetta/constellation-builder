c3c39104ebbacbfddf3815c03c958bfc11d39c4a
brewchetta <chett.tiller@gmail.com>
2020-09-23 12:36:59 -0400

    Changed instructions to reflect functionality

--- src/components/ControlsDisplay.js
+++ src/components/ControlsDisplay.js
@@ -8,19 +8,23 @@ function ControlsDisplay() {
     <>
       <span>Controls (click to close):</span>
       <br/>
+      <br/>
 
       <span>Click empty space to add a point</span>
+      <br/>
       <br/>
 
       <span>Clicking a point will create a line between it and another point or empty space</span>
       <br/>
+      <br/>
 
-      <span>CTRL + Z to undo the last line</span>
+      <span>CTRL + Z to undo the last line or deselect the currently selected point</span>
+      <br/>
       <br/>
     </>
   )
 
-  const closedControls = () => <span>Click to open controls</span>
+  const closedControls = () => <span>Controls (click to open)</span>
 
   return (
     <div onClick={() => setOpen(!open)}>
