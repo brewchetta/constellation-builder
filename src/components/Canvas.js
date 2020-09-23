b72856e7f5a57b1e9e5b5ccaedb59c660de29a6f
brewchetta <chett.tiller@gmail.com>
2020-09-23 09:44:32 -0400

    Added functionality to click a point and add it as the current point

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,15 +6,23 @@ function Canvas() {
 
   const [points, setPoints] = useState([])
 
+  const [currentPoint, setCurrentPoint] = useState({})
+
   const handleClick = (e) => {
     const x = e.clientX
     const y = e.clientY
-    setPoints([...points, {x,y}])
+    if (!points.find(p => Math.abs(p.x - x) < 10 && Math.abs(p.y - y) < 10)) {
+      setPoints([...points, {x,y}])
+      setCurrentPoint({})
+    }
   }
 
+  const handlePointClick = (point) => {
+    setCurrentPoint(point)
+  }
 
   const displayPoints = () => {
-    return points.map((point,i) => <Point key={i} point={point} />)
+    return points.map((point,i) => <Point key={i} handlePointClick={handlePointClick} point={point} />)
   }
 
   const displayLines = () => {
@@ -26,6 +34,8 @@ function Canvas() {
 
   return (
     <div id="canvas" onClick={handleClick}>
+
+      <span style={{color: 'white'}}>currentPoint: {currentPoint.x}, {currentPoint.y}</span>
 
       {displayPoints()}
       {displayLines()}
--- src/components/Point.js
+++ src/components/Point.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Point({point}) {
+function Point({point, handlePointClick}) {
 
   return (
     <div className="point-center" style={{top: point.y, left: point.x}}>
-      <div className="point-display" />
+      <div className="point-display" onClick={() => handlePointClick(point)} />
     </div>
   )
 
