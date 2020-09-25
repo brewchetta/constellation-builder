cf3ff53e7970d4c47292d69610fea449247b9dcc
brewchetta <chett.tiller@gmail.com>
2020-09-24 21:55:44 -0400

    Changed index to utilize new redux service

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './redux/reducer'
 
-const store = createStore(reducer)
-
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={reducer}>
       <App />
     </Provider>,
   document.getElementById('root')
