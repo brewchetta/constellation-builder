9f17685c71ce91941f88fe503c8151ce869267d0
brewchetta <chett.tiller@gmail.com>
2020-09-24 21:55:14 -0400

    Added line and point class, added redux to line

---
+++ src/classes/Line.js
@@ -0,0 +1,31 @@
+import store from '../redux/reducer'
+
+import { setLines } from '../redux/actions'
+
+store.subscribe(() => console.log(store.getState()))
+
+export default class Line {
+  constructor(points) {
+    this.points = points
+    store.dispatch(setLines([...Line.getLines(), this]))
+  }
+
+  static getLines = () => store.getState().lines
+
+  midPoint = () => {
+    const x = Math.abs(this.points[0].x - this.points[1].x) / 2 + Math.min(this.points[0].x, this.points[1].x)
+    const y = Math.abs(this.points[0].y - this.points[1].y) / 2 + Math.min(this.points[0].y, this.points[1].y)
+    return {x, y}
+  }
+
+  width = () => {
+    const distX = this.points[0].x - this.points[1].x
+    const distY = this.points[0].y - this.points[1].y
+    return Math.sqrt((distX ** 2) + (distY ** 2)) - 10
+  }
+
+  angle = () => {
+    return Math.atan2(this.points[1].y - this.points[0].y, this.points[1].x - this.points[0].x) * 180 / Math.PI
+  }
+
+}
---
+++ src/classes/Point.js
@@ -0,0 +1,9 @@
+class Point {
+  constructor({x, y, name, description}) {
+    this.x = x
+    this.y = y
+    this.name = name
+    this.description = description
+    console.log(this)
+  }
+}
